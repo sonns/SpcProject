a90c1c002f80732e7fd1e55dbcbf7ff983758d0b
son <truongsonns@gmail.com>
2016-09-17 23:21:05 +0700

    Makeup language

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -49,7 +49,7 @@ class AppController extends Controller
         parent::initialize();
         $this->loadComponent('RequestHandler');
         $this->loadComponent('Cookie');
-        $this->_init_language();
+
     }
     public function beforeFilter(Event $event)
     {
@@ -72,42 +72,6 @@ class AppController extends Controller
         $this->set('theme', Configure::read('Admin'));
     }
 
-
-    private function _init_language(){
-        $this->set('ddlLanguage', Configure::read('language'));
-        $language = !isset($this->request->query['language'])   ?   $this->request->session()->read('Config.language')
-            :   $this->request->query['language'];
-        switch($language)
-        {
-            case "du":
-                I18n::locale('du_DU');
-                break;
-            case "ru":
-                I18n::locale('ru_RU');
-                break;
-            case "jp":
-                I18n::locale('ja_JP');
-                break;
-            default:
-                I18n::locale('en_US');
-                break;
-        }
-    }
-
-
-    private function _setLanguage()
-    {
-
-        $session = $this->request->session();
-        if ($this->Cookie->read('language') && !$session->check('Config.language')) {
-            $session->write('Config.language', $this->Cookie->read('language'));
-        }
-        else if (isset($this->request->query['language']) && ($this->request->query['language'] !=  $session->read('Config.language'))) {
-            $session->write('Config.language', $this->request->query['language']);
-            $this->Cookie->write('language', $this->request->query['language'], false, '20 days');
-        }
-
-    }
 
 
 }
