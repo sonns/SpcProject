c05af77da36e9566dd938b7046e45d8ceb80d8c1
Son Nguyen <son.nguyen@spc-vn.com>
2016-10-28 16:05:17 +0700

    no message

--- plugins/Notification/webroot/WebSocket/app.js
+++ plugins/Notification/webroot/WebSocket/app.js
@@ -3,31 +3,7 @@
  */
 
 var http = require('http');
-// var dispatcher = require('httpdispatcher');
-function handleRequest(request, response){
-    try {
-        // response.writeHead(200, {"Content-Type": "text/html"});
-        // response.write("<!DOCTYPE \"html\">");
-        // response.write("<html>");
-        // response.write("<head>");
-        // response.write("<title>Hello World Page</title>");
-        // response.write("</head>");
-        // response.write("<body>");
-        // response.write("Hello World!");
-        // response.write("</body>");
-        // response.write("</html>");
-        // response.end();
-    } catch(err) {
-        console.log(err);
-    }
-}
-
-//Create a server
-var servidor = http.createServer(handleRequest);
-
-
-// var servidor = http.createServer();
-
+var servidor = http.createServer();
 var io = require('socket.io').listen(servidor);
 var port = 5000;
 
@@ -40,5 +16,5 @@ io.sockets.on('connection', function(socket)
 });
 
 servidor.listen(port, function(){
-    // console.log("http://localhost: " + port);
+    console.log("http://localhost: " + port);
 });
